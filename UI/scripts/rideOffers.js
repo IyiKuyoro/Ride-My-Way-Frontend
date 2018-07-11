5a8703bdabf1c3adead12938fc0639a2cc2e5d19
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-07-11 23:21:03 +0100

    Consume the get rides endpoint

--- UI/scripts/rideOffers.js
+++ UI/scripts/rideOffers.js
@@ -14,3 +14,33 @@ closeDetails.addEventListener('click', () => {
   rideDetails.classList.toggle('visibility');
   screen[0].classList.toggle('visibility');
 });
+
+const rides = document.getElementById('rides');
+
+fetch('https://iyikuyoro-ride-my-way.herokuapp.com/api/v1/rides', {
+  method: 'GET',
+  headers: {
+    jwt: sessionStorage.token
+  }
+})
+  .then(res => res.json())
+  .then((data) => {
+    for (let i = 0; i < 16; i += 1) {
+      rides.innerHTML += `<div class="ride-card">
+        <div class= "direction">
+          <h5>${data.data.rides[i].origin}</h5>
+          <img src="./images/icon_to.png">
+          <h5>${data.data.rides[i].destination}</h5>
+        </div>
+        <div class="time">
+          <img src="./images/icon_clock.png">
+          <h6>${data.data.rides[i].time}</h6>
+        </div>
+        <div class="seats">
+          <img src="./images/icon_seat.png">
+          <h6>${data.data.rides[i].avaliableSpace}</h6>
+        </div>
+      </div>`;
+    }
+  })
+  .catch(err => console.log(err));
