0c4d4e52e541e6c02eda84f07bd5747fbac25c54
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-07-18 16:28:21 +0100

    feat(UI): Consume put request
    - Add feature to consume put request
    
    [Delivers #158925532]

--- UI/scripts/requests.js
+++ UI/scripts/requests.js
@@ -8,13 +8,63 @@ const addRequest = (data) => {
 
   for (let i = 0; i < data.data.requests.length; i += 1) {
     htmlString += `<li><span class="bold">${data.data.requests[i].requesterName} </span><span>${data.data.requests[i].mobileNumber}</span></li>
-    <div class="wrap-container">
-      <button id="accept" class="response-buttons">Accept</button>
-      <button id="decline" class="response-buttons">Decline</button>
+    <div id="${data.data.requests[i].id}" class="wrap-container">
+      <button class="response-buttons accept">Accept</button>
+      <button class="response-buttons decline">Decline</button>
     </div>`;
   }
 
   return htmlString;
+};
+
+const addResponseEventListners = (rideId) => {
+  const accept = document.getElementsByClassName('accept');
+  const decline = document.getElementsByClassName('decline');
+
+  for (let i = 0; i < accept.length; i += 1) {
+    accept[i].addEventListener('click', () => {
+      const post = {
+        newStatus: 'accepted'
+      };
+
+      fetch(`https://iyikuyoro-ride-my-way.herokuapp.com/api/v1/users/rides/${rideId}/requests/${accept[i].parentElement.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          jwt: sessionStorage.token
+        },
+        body: JSON.stringify(post)
+      })
+        .then(res => res.json())
+        .then((data) => {
+          const message = document.getElementById('request-message');
+          message.innerHTML = data.message;
+        })
+        .catch(err => console.log(err));
+    });
+
+    decline[i].addEventListener('click', () => {
+      const post = {
+        newStatus: 'declined'
+      };
+
+      console.log(decline[i].parentElement);
+      fetch(`https://iyikuyoro-ride-my-way.herokuapp.com/api/v1/users/rides/${rideId}/requests/${decline[i].parentElement.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          jwt: sessionStorage.token
+        },
+        body: JSON.stringify(post)
+      })
+        .then(res => res.json())
+        .then((data) => {
+          const message = document.getElementById('request-message');
+          message.innerHTML = data.message;
+        })
+        .catch(err => console.log(err));
+    });
+  }
 };
 
 const getRequests = (rideId) => {
@@ -27,15 +77,17 @@ const getRequests = (rideId) => {
     .then(res => res.json())
     .then((data) => {
       screen.classList.toggle('visibility');
-      screen.innerHTML = `<div id="ridedetails">          
-      <img id="close" src="./images/icon_close.png">
-      <div>
-        <h3>Requests</h3>
-      </div>
-      <ul>
-        ${addRequest(data)}
-      </ul>
-    </div>`;
+      screen.innerHTML = `<div id="request-message"></div>
+      <div id="ridedetails">          
+        <img id="close" src="./images/icon_close.png">
+        <div>
+          <h3>Requests</h3>
+        </div>
+        <ul>
+          ${addRequest(data)}
+        </ul>
+      </div>`;
+      addResponseEventListners(rideId);
 
       const close = document.getElementById('close');
       close.addEventListener('click', () => {
