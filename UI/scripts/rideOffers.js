c09fccf9063e5a0878fe960a59ebae3ecf83d0b6
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-07-20 16:42:54 +0100

    Chore(UI): Implement rides sorting
    > - Add all rides origins to the from selector
    > - Add all rides destinations to the to selector
    > - Sort rides based on the selcted orgin and destinations
    > - Remove all irrelavant files from folder
    
    [Task2 #159144479]

--- UI/scripts/rideOffers.js
+++ UI/scripts/rideOffers.js
@@ -5,8 +5,13 @@ const signOut = document.getElementById('sign-out');
 const screen = document.getElementsByClassName('screen')[0];
 const backButton = document.getElementById('back');
 const nextButton = document.getElementById('next');
+const fromFilter = document.querySelector('.from-filter');
+const toFilter = document.querySelector('.to-filter');
 
 let savedRides = []; // Array to hold ride offers returned by the server
+let filteredRides = []; // Array to hold filtered rides
+const origins = {};
+const destinations = {};
 let noOfPages = 0; // The max number of pages
 let curPage = 0;
 
@@ -93,28 +98,28 @@ const addViewRideEventHandler = () => {
   }
 };
 
-const displayRides = () => {
+const displayRides = (ridesArray) => {
   rides.innerHTML = '';
-  noOfPages = Math.ceil(savedRides.length / 16);
+  noOfPages = Math.ceil(ridesArray.length / 16);
   const firstRideOnPage = ((curPage - 1) * 16);
-  const noOfRidesLeft = savedRides.length - firstRideOnPage;
+  const noOfRidesLeft = ridesArray.length - firstRideOnPage;
 
   // Load 16 rides to the page
   for (let i = firstRideOnPage; i < (noOfRidesLeft >= 16 ? (16 + firstRideOnPage) : (noOfRidesLeft + firstRideOnPage)); i += 1) {
-    rides.innerHTML += `<div id="${savedRides[i].id}" class="ride-card wrap-container">
+    rides.innerHTML += `<div id="${ridesArray[i].id}" class="ride-card wrap-container">
       <div class= "direction wrap-container">
-        <h5>${savedRides[i].origin}</h5>
+        <h5>${ridesArray[i].origin}</h5>
         <img src="./images/icon_to.png">
-        <h5>${savedRides[i].destination}</h5>
+        <h5>${ridesArray[i].destination}</h5>
       </div>
       <div class="details wrap-container">
         <div class="time">
           <img src="./images/icon_clock.png">
-          <h6>${savedRides[i].time}</h6>
+          <h6>${ridesArray[i].time}</h6>
         </div>
         <div class="seats">
           <img src="./images/icon_seat.png">
-          <h6>${savedRides[i].avaliableSpace}</h6>
+          <h6>${ridesArray[i].avaliableSpace}</h6>
         </div>
       </div>
       <button class="view">View</button>
@@ -122,6 +127,30 @@ const displayRides = () => {
   }
   addViewRideEventHandler();
   displayPages();
+};
+
+const saveOriginsAndDestinations = () => {
+  savedRides.forEach((element) => {
+    if (!origins[element.origin]) {
+      const opt = document.createElement('option');
+
+      opt.text = element.origin;
+      opt.value = element.origin;
+
+      fromFilter.add(opt);
+      origins[element.origin] = 1;
+    }
+
+    if (!destinations[element.destination]) {
+      const opt = document.createElement('option');
+
+      opt.text = element.destination;
+      opt.value = element.destination;
+
+      toFilter.add(opt);
+      destinations[element.destination] = 1;
+    }
+  });
 };
 
 // Get all rides from the server
@@ -138,10 +167,11 @@ fetch('https://iyikuyoro-ride-my-way.herokuapp.com/api/v1/rides', {
       window.location.replace('index.html');
     } else {
       cover.style.display = 'none'; // Remove the cover screen and load all the rides into an array
-
       savedRides = data.data.rides;
+      filteredRides = savedRides;
+      saveOriginsAndDestinations();
       curPage = 1;
-      displayRides(); // Display the first page of rides
+      displayRides(filteredRides); // Display the first page of rides
     }
   })
   .catch(err => console.log(err));
@@ -155,10 +185,42 @@ signOut.addEventListener('click', (event) => {
 
 backButton.addEventListener('click', () => {
   curPage -= 1;
-  displayRides();
+  displayRides(filteredRides);
 });
 
 nextButton.addEventListener('click', () => {
   curPage += 1;
-  displayRides();
+  displayRides(filteredRides);
+});
+
+const filterFunction = () => {
+  filteredRides = [];
+
+  savedRides.forEach((element) => {
+    if (fromFilter.value !== 'all' && toFilter.value !== 'all') {
+      if (element.origin === fromFilter.value && element.destination === toFilter.value) {
+        filteredRides.push(element);
+      }
+    } else if (fromFilter.value === 'all' && toFilter.value !== 'all') {
+      if (element.destination === toFilter.value) {
+        filteredRides.push(element);
+      }
+    } else if (fromFilter.value !== 'all' && toFilter.value === 'all') {
+      if (element.origin === fromFilter.value) {
+        filteredRides.push(element);
+      }
+    } else if (fromFilter.value === 'all' && toFilter.value === 'all') {
+      filteredRides.push(element);
+    }
+  });
+
+  curPage = 1;
+  displayRides(filteredRides);
+};
+
+fromFilter.addEventListener('input', () => {
+  filterFunction();
+});
+toFilter.addEventListener('input', () => {
+  filterFunction();
 });
